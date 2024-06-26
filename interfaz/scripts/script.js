af05290826cfca82e4a0c475fd716ad3d617b752
Matheus Hladki <Matheushladki5@gmail.com>
2024-06-26 11:18:45 -0300

    Creacion de search bar y movimiento al posicionar el mouse sobre la carta ("producto")

--- interfaz/scripts/script.js
+++ interfaz/scripts/script.js
@@ -2,3 +2,7 @@ $(".menu-btn").click(function() {
     $(".navbar .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
 });
+
+$(".enter").click(function(){
+    alert("Buscar...")
+})
