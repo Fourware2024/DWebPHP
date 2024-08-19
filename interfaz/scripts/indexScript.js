fa3af5ec290e1f23a46c9662249169d75e5a951e
Rogueair <matheushladki5@gmail.com>
2024-08-19 17:49:52 -0300

    cambios en el index (Sliders)

--- interfaz/scripts/indexScript.js
+++ interfaz/scripts/indexScript.js
@@ -6,3 +6,52 @@ $(".menu-btn").click(function() {
 $(".enter").click(function(){
     alert("Buscar...")
 })
+
+
+const imgs = [
+    '../carruselTop/img1.png',
+    '../carruselTop/img2.png'
+];
+let imgPos = 0;
+
+$("#arrow-banner-left").click(previousImg);
+$("#arrow-banner-right").click(nextImg);
+
+function nextImg() {
+    if(imgPos >= imgs.length-1) {
+        imgPos = 0;
+    }else{
+        imgPos++;
+    }
+    $("#carrusel-inner").css('background-image','url(' + imgs[imgPos] + ')');
+}
+
+function previousImg() {
+    if(imgPos < imgs.length-1) {
+        imgPos = imgs.length-1;
+    }else{
+        imgPos--;
+    }
+    $("#carrusel-inner").css('background-image','url(' + imgs[imgPos] + ')');
+}
+
+var offers = new Splide( '.offers', {
+    perPage: 5,
+    rewind : true,
+    speed: 1000,
+    padding: '1%',
+    gap: '100px',
+    arrow: {prev: '.splide__arrow--prev', next: '.splide__arrow--next'},
+  } );
+
+  var sellers = new Splide( '.sellers', {
+    perPage: 5,
+    rewind : true,
+    speed: 1000,
+    padding: '1%',
+    gap: '100px',
+    arrow: {prev: '.splide__arrow--prev', next: '.splide__arrow--next'},
+  } );
+
+  offers.mount();
+  sellers.mount();
